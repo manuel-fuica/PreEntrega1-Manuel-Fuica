cb740cddd18727082193175a48aed465ce076ffe
Manuel Fuica <manuelfuica13@gmail.com>
2024-04-25 08:51:51 -0400

    avances para la entrega, modificaciones alert

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-// Array para almacenar los productos en el carrito
+// Array para almacenar los productos en el carrito de compras
 const carrito = [];
 
-// Función para agregar un producto al array de carrito
+// Función para agregar un producto al array del carrito
 function agregarProducto() {
-    const producto = parseInt(prompt("Elige el producto que deseas comprar:\n1. Poleras ($5000)\n2. Zapatillas ($10000)\n3. Pantalones ($8000)\n4. Shorts ($4000)"));
+    const producto = parseInt(prompt("Elige el producto que deseas comprar:\n1. Poleras valor: $ 5.000\n2. Zapatillas valor: $ 10.000\n3. Pantalones valor: $ 8.000\n4. Shorts  valor: $ 4.000"));
     const cantidad = parseInt(prompt("Ingresa la cantidad que deseas"));
     let precioFinal = cantidad;
 
@@ -25,13 +25,13 @@ function agregarProducto() {
             return;
     }
 
-    const productoSeleccionado = {
+    const productoSeleccionado = {//creacion de objeto
         nombre: "",
         precio: precioFinal,
         cantidad: cantidad
     };
     
-    switch (producto) {
+    switch (producto) {//creacion de switch para el nombre del producto que elija el usuario
         case 1:
             productoSeleccionado.nombre = "Poleras";
             break;
@@ -50,7 +50,7 @@ function agregarProducto() {
     }
 
     carrito.push(productoSeleccionado);
-    alert("Producto agregado al carrito: " + productoSeleccionado.nombre + " - Precio: $" + productoSeleccionado.precio + " - Cantidad: " + productoSeleccionado.cantidad);
+    alert("Producto agregado al carrito: \n" + " " + productoSeleccionado.nombre + " \n Cantidad: " + productoSeleccionado.cantidad + "\n Total a pagar: $" + productoSeleccionado.precio);
 }
 
 function devolverProducto() {
@@ -59,30 +59,36 @@ function devolverProducto() {
     const nombre = prompt("Ingrese su nombre completo");
     const telefono = prompt("Ingrese su teléfono");
 
-    alert("Estimad@ " + nombre + "\nPronto uno de nuestros ejecutivos se contactará con usted para coordinar la devolución del producto. Muchas gracias");
+    alert("Estimad@ " + nombre + "\nPronto uno de nuestros ejecutivos se contactará con usted, al numero de telefono " + telefono + " para coordinar la devolución del producto.\n Muchas gracias");
 }
 
 function mostrarCarrito() {
-    if (carrito.length === 0) {
-        alert("El carrito está vacío.");
-    } else {
-        let total = 0;
-        let mensaje = "Productos en el carrito:\n";
-        carrito.forEach((producto, index) => {
-            mensaje += `${index + 1}. ${producto.nombre} - Precio: $${producto.precio} - Cantidad: ${producto.cantidad}\n`;
-            total += producto.precio * producto.cantidad;
-        });
-        mensaje += `Total a pagar: $${total}`;
-        alert(mensaje);
+    var preciosTotales = {};
+    for (var i = 0; i < carrito.length; i++) {
+        var producto = carrito[i];
+        if (preciosTotales[producto.nombre]) {
+            preciosTotales[producto.nombre] += producto.precio * producto.cantidad;
+        } else {
+            preciosTotales[producto.nombre] = producto.precio * producto.cantidad;
+        }
     }
+    var total = 0;
+    var mensaje = "Productos en el carrito:\n";
+    for (var nombre in preciosTotales) {
+        var precioTotal = preciosTotales[nombre];
+        mensaje += nombre + ": " + precioTotal.toFixed(2) + "\n";
+        total += precioTotal;
+    }
+    mensaje += "\nPrecio total: " + total.toFixed(2);
+    alert(mensaje);
 }
 
 function pagar() {
     if (carrito.length === 0) {
         alert("No hay productos en el carrito.");
     } else {
-        alert("Pago exitoso. Muchas gracias por su compra.");
-        carrito.length = 0; // Vaciar el carrito después de pagar
+        alert("Pago exitoso.\n Muchas gracias por su compra.");
+        carrito.length = 0; // vacia el carrito para cerrar el programa
         return;
     }
 }
